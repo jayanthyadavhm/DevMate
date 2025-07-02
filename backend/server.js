77fdb15e2853d5cd8818f912a162153f9017f5fb
channrama <dschannappa93@gmail.com>
2025-07-02 23:01:23 +0530

    updated the backend,mongo db url

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,7 @@ app.use('/api/tasks', taskRoutes);
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
